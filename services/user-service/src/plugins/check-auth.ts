a42b4c14a0bbbafe6f0d391995ca7e6581d937da
Vu Van Dung <me@joulev.dev>
2024-09-11 02:53:39 +0800

    type-safe environment variables (2)
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- services/user-service/src/plugins/check-auth.ts
+++ services/user-service/src/plugins/check-auth.ts
@@ -1,16 +1,12 @@
 import { jwt } from "@elysiajs/jwt";
 import { db } from "@peerprep/db";
+import { env } from "@peerprep/env";
 import Elysia from "elysia";
-
-if (!process.env.JWT_SECRET) {
-  console.error("JWT_SECRET environment variable is required.");
-  process.exit(1);
-}
 
 // name is necessary for Elysia to deduplicate the plugin
 // https://elysiajs.com/essential/plugin.html#plugin-deduplication
 export const checkAuthPlugin = new Elysia({ name: "check-auth" })
-  .use(jwt({ name: "jwt", secret: process.env.JWT_SECRET }))
+  .use(jwt({ name: "jwt", secret: env.JWT_SECRET }))
   .derive({ as: "scoped" }, async ({ jwt, cookie: { auth_token } }) => {
     if (!auth_token) return { user: null };
 
