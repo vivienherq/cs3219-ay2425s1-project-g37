b743cb5fec60a259d70786f091fb1f25ee95ac9e
Vu Van Dung <me@joulev.dev>
2024-09-11 22:44:40 +0800

    change casing of file names
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/peerprep/src/app.tsx
+++ apps/peerprep/src/app.tsx
@@ -1,7 +1,7 @@
 import viteLogo from "/vite.svg";
 import { useState } from "react";
 
-import "./App.css";
+import "./app.css";
 import reactLogo from "./assets/react.svg";
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
