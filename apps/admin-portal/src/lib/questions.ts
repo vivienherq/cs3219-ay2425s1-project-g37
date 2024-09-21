9e353aae86323911cc0454b50efa90929498c212
Vu Van Dung <me@joulev.dev>
2024-09-22 04:33:16 +0800

    add dummy questions to work with
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/admin-portal/src/lib/questions.ts
+++ apps/admin-portal/src/lib/questions.ts
@@ -19,7 +19,32 @@ async function questionsFetcher() {
 
 export const SWR_KEY_QUESTIONS = "questions";
 
+const dummyData: Question[] = [
+  {
+    id: "dummy1",
+    title: "Dummy Question 1",
+    content: "Hello _World_",
+    difficulty: "EASY",
+    tags: ["tag1", "tag2"],
+    leetCodeLink: "https://leetcode.com",
+    createdAt: new Date(0),
+    updatedAt: new Date(0),
+  },
+  {
+    id: "dummy2",
+    title: "Dummy Question 2",
+    content: "Hello **World**",
+    difficulty: "MEDIUM",
+    tags: ["tag3", "tag4"],
+    leetCodeLink: "https://leetcode.com",
+    createdAt: new Date(0),
+    updatedAt: new Date(0),
+  },
+];
+
 export function useQuestions(): SWRHookResult<Question[]> {
   const { data } = useSWR(SWR_KEY_QUESTIONS, questionsFetcher);
-  return data === undefined ? { data: undefined, isLoading: true } : { data, isLoading: false };
+  return data === undefined
+    ? { data: undefined, isLoading: true }
+    : { data: [...dummyData, ...data], isLoading: false };
 }
