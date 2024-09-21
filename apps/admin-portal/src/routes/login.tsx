6ccc7d3ca299b5f588f03a85e8640014584c55ea
Vu Van Dung <me@joulev.dev>
2024-09-21 19:22:22 +0800

    login almost working
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/admin-portal/src/routes/login.tsx
+++ apps/admin-portal/src/routes/login.tsx
@@ -1,18 +1,36 @@
 import { Button } from "@peerprep/ui/button";
 import { TextInput } from "@peerprep/ui/text-input";
+import { getKyErrorMessage, userClient } from "@peerprep/utils/client";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import { mutate } from "swr";
+
+import { SWR_KEY_USER } from "~/lib/auth";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const [pending, setPending] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
     <div className="grid h-screen w-screen place-items-center">
       <form
         className="bg-main-900 flex w-full max-w-lg flex-col gap-6 p-12"
-        onSubmit={e => {
+        onSubmit={async e => {
           e.preventDefault();
-          // log in
+          if (pending) return;
+          setPending(true);
+          try {
+            await userClient.post("auth/login", { json: { email, password } });
+            await mutate(SWR_KEY_USER);
+            toast.success("Welcome back!");
+            navigate("/");
+          } catch (e) {
+            toast.error(getKyErrorMessage(e));
+          }
+          setPending(false);
         }}
       >
         <h1 className="text-main-50 text-2xl">Login</h1>
