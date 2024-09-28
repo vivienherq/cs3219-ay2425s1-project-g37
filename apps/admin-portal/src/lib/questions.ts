ce7e15f320b538de9b9e0b06bc65957d2578d711
Vu Van Dung <me@joulev.dev>
2024-09-28 15:14:56 +0800

    add swr hooks for edit and deletion of questions as well
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/admin-portal/src/lib/questions.ts
+++ apps/admin-portal/src/lib/questions.ts
@@ -1,4 +1,4 @@
-import type { NewQuestion, Question } from "@peerprep/schemas";
+import type { NewQuestion, Question, UpdateQuestion } from "@peerprep/schemas";
 import { questionsClient } from "@peerprep/utils/client";
 import useSWR, { mutate } from "swr";
 import useSWRMutation from "swr/mutation";
@@ -22,5 +22,18 @@ export async function mutateQuestion(id: string) {
 export function useAddQuestions() {
   return useSWRMutation("questions:/", (_, { arg }: { arg: NewQuestion | NewQuestion[] }) =>
     questionsClient.post("/", { json: arg }),
+  );
+}
+
+export function useEditQuestion(id: string) {
+  return useSWRMutation(
+    ["questions:/", `questions:/${id}`],
+    (_, { arg }: { arg: UpdateQuestion }) => questionsClient.patch(`/${id}`, { json: arg }),
+  );
+}
+
+export function useDeleteQuestion(id: string) {
+  return useSWRMutation(["questions:/", `questions:/${id}`], () =>
+    questionsClient.delete(`/${id}`),
   );
 }
