06adce0a6e6e989f568fbe0fd02270ad5d275401
Vu Van Dung <me@joulev.dev>
2024-09-29 14:53:18 +0800

    implement soft delete for the question table
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- services/questions-service/src/controllers/questions.ts
+++ services/questions-service/src/controllers/questions.ts
@@ -3,37 +3,75 @@ import type { NewQuestion, Question, UpdateQuestion } from "@peerprep/schemas";
 import { ExpectedError } from "@peerprep/utils/server";
 import { StatusCodes } from "http-status-codes";
 
-export async function createQuestions(questions: NewQuestion | NewQuestion[]) {
+async function createQuestion(question: NewQuestion) {
   try {
-    await db.question.createMany({ data: questions });
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002")
-      throw new ExpectedError("Question ID already exists", StatusCodes.CONFLICT);
-    throw error;
+    const existingQuestion = await db.question.findUnique({
+      where: { leetCodeLink: question.leetCodeLink },
+    });
+    if (existingQuestion && !existingQuestion.deletedAt) return false;
+    if (existingQuestion)
+      await db.question.update({
+        where: { id: existingQuestion.id },
+        data: { ...question, deletedAt: null },
+      });
+    else await db.question.create({ data: question });
+    return true;
+  } catch {
+    return false;
   }
 }
 
+export async function createQuestions(questions: NewQuestion | NewQuestion[]) {
+  const questionsArray = Array.isArray(questions) ? questions : [questions];
+  const result = await Promise.all(questionsArray.map(createQuestion));
+  if (result.every(x => !x))
+    throw new ExpectedError("All provided question(s) already exist", StatusCodes.CONFLICT);
+  const countSuccess = result.filter(x => x).length;
+  if (countSuccess === result.length) return "All questions successfully saved!";
+  return `${countSuccess} question(s) saved, ${result.length - countSuccess} question(s) couldn't be saved due to conflicts`;
+}
+
 export async function getQuestion(id: string): Promise<Question> {
-  const question = await db.question.findUnique({ where: { id } });
-  if (!question) throw new ExpectedError(`Question ${id} not found`, StatusCodes.NOT_FOUND);
+  const question = await db.question.findUnique({
+    where: { id, OR: [{ deletedAt: { isSet: false } }, { deletedAt: { equals: null } }] },
+  });
+  if (!question) throw new ExpectedError("Question not found", StatusCodes.NOT_FOUND);
   return question;
 }
 
 export async function getAllQuestions(): Promise<Question[]> {
-  return db.question.findMany();
+  return db.question.findMany({
+    where: { OR: [{ deletedAt: { isSet: false } }, { deletedAt: { equals: null } }] },
+    orderBy: { title: "asc" },
+  });
 }
 
 export async function updateQuestion(id: string, question: UpdateQuestion) {
   try {
-    await db.question.update({ where: { id }, data: question });
+    await db.question.update({
+      where: { id, OR: [{ deletedAt: { isSet: false } }, { deletedAt: { equals: null } }] },
+      data: question,
+    });
   } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002")
-      throw new ExpectedError("A clash with another question occurred", StatusCodes.CONFLICT);
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2025")
+      throw new ExpectedError(
+        "The question to be updated could not be found",
+        StatusCodes.NOT_FOUND,
+      );
     throw error;
   }
 }
 
-export async function deleteQuestion(id: string) {
-  const question = await db.question.delete({ where: { id } });
-  if (!question) throw new ExpectedError(`Question ${id} not found`, StatusCodes.NOT_FOUND);
+export async function deleteQuestion(id: string, opts?: { dangerouslyHardDelete?: boolean }) {
+  try {
+    if (opts?.dangerouslyHardDelete) await db.question.delete({ where: { id } });
+    else await db.question.update({ where: { id }, data: { deletedAt: new Date() } });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2025")
+      throw new ExpectedError(
+        "The question to be deleted could not be found",
+        StatusCodes.NOT_FOUND,
+      );
+    throw error;
+  }
 }
