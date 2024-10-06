4b92f4a4558f420ebd75c9c93569f1da05e0d32a
Vu Van Dung <me@joulev.dev>
2024-10-06 15:45:47 +0800

    hopefully fix malfunctioning cookies in self host
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- services/user-service/src/lib/get-jwt.ts
+++ services/user-service/src/lib/get-jwt.ts
@@ -1,13 +1,16 @@
 import type { JWTPayloadSpec } from "@elysiajs/jwt";
 import { env } from "@peerprep/env";
+import type { ElysiaCookie } from "elysia/cookies";
 
 export async function getJwt(
   id: string,
   sign: (morePayload: Record<string, string | number> & JWTPayloadSpec) => Promise<string>,
-) {
+): Promise<Partial<ElysiaCookie>> {
   return {
     value: await sign({ sub: id }),
     httpOnly: true,
+    secure: env.VITE_SELF_HOST,
+    sameSite: "none",
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // 1 month
     domain: env.VITE_SELF_HOST ? ".joulev.dev" : undefined,
   };
