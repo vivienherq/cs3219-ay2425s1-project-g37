8de993f626b038e1885cb304fd9402eaa75edcca
Vu Van Dung <me@joulev.dev>
2024-10-11 17:00:36 +0800

    set up the matching queue mechanism
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/peerprep/src/routes/index.tsx
+++ apps/peerprep/src/routes/index.tsx
@@ -1,26 +1,25 @@
+import { env } from "@peerprep/env";
 import { useState } from "react";
 import useSWRSubscription, { type SWRSubscriptionOptions } from "swr/subscription";
 
 export default function IndexPage() {
   const [ws, setWs] = useState<WebSocket | null>(null);
   const { data } = useSWRSubscription("data", (_, { next }: SWRSubscriptionOptions<string>) => {
-    const ws = new WebSocket("ws://localhost:3004");
+    const ws = new WebSocket(`ws://localhost:${env.VITE_MATCHING_SERVICE_PORT}`);
     setWs(ws);
     ws.addEventListener("message", event => next(null, event.data));
     ws.addEventListener("error", () => next("Connection error", undefined));
     return () => {
-      if (ws.readyState === 1) {
-        ws.close();
-      } else {
-        ws.addEventListener("open", () => {
-          ws.close();
-        });
-      }
+      if (ws.readyState === 1) ws.close();
+      else ws.addEventListener("open", () => ws.close());
     };
   });
+  if (!ws) return null;
   return (
     <div>
-      <button onClick={() => ws?.send(new Date().toISOString())}>Click me</button>
+      <button onClick={() => ws.send(JSON.stringify({ difficulty: "HARD", tags: [] }))}>
+        Click me
+      </button>
       <div>{data ?? "nothingness"}</div>
     </div>
   );
--- services/matching-service/src/index.ts
+++ services/matching-service/src/index.ts
@@ -1,10 +1,39 @@
+import { db } from "@peerprep/db";
 import { env } from "@peerprep/env";
+import type { Difficulty } from "@peerprep/schemas";
+import { questions } from "@peerprep/schemas/validators";
 import {
+  ExpectedError,
   elysiaAuthPlugin,
   elysiaCorsPlugin,
   elysiaFormatResponsePlugin,
 } from "@peerprep/utils/server";
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { StatusCodes } from "http-status-codes";
+
+import type { WorkerResponse } from "./worker";
+
+async function getQuestionsFromFilter(difficulty: Difficulty, tags: string[]) {
+  const questions = await db.question.findMany({
+    where: { difficulty, tags: tags.length > 0 ? { hasSome: tags } : undefined },
+  });
+  return questions;
+}
+
+const worker = new Worker("src/worker.ts");
+worker.addEventListener("message", ({ data }: { data: WorkerResponse }) => {
+  switch (data.type) {
+    case "success": {
+      sendMessage(data.matched[0], { type: "success" });
+      sendMessage(data.matched[1], { type: "success" });
+      break;
+    }
+    case "timeout": {
+      sendMessage(data.userId, { type: "timeout" });
+      break;
+    }
+  }
+});
 
 const app = new Elysia()
   .use(elysiaCorsPlugin)
@@ -12,14 +41,37 @@ const app = new Elysia()
   .use(elysiaAuthPlugin)
   .get("/status", () => new Response("Online"))
   .ws("/", {
+    body: t.Object({
+      difficulty: questions.difficultySchema,
+      tags: t.Array(t.String({ minLength: 1 })),
+    }),
     open(ws) {
-      ws.send("Welcome!");
+      if (!ws.data.user) throw new ExpectedError("You must be logged in", StatusCodes.UNAUTHORIZED);
+      ws.subscribe(ws.data.user.id);
     },
-    message(ws, message) {
-      console.log(ws.id);
-      ws.send(`${message} ${ws.data.user?.email ?? "unauthenticated"}`);
+    async message(ws, { difficulty, tags }) {
+      if (!ws.data.user) throw new ExpectedError("You must be logged in", StatusCodes.UNAUTHORIZED);
+      const questions = await getQuestionsFromFilter(difficulty, tags);
+      worker.postMessage({
+        type: "add",
+        userId: ws.data.user.id,
+        questionIds: questions.map(q => q.id),
+      });
+      sendMessage(ws.data.user.id, { type: "acknowledgement" });
+    },
+    close(ws) {
+      if (!ws.data.user) throw new ExpectedError("You must be logged in", StatusCodes.UNAUTHORIZED);
+      ws.unsubscribe(ws.data.user.id);
+      worker.postMessage({ type: "remove", userId: ws.data.user.id });
     },
   })
   .listen(env.VITE_MATCHING_SERVICE_PORT);
+
+function sendMessage(
+  userId: string,
+  message: { type: "success" } | { type: "acknowledgement" } | { type: "timeout" },
+) {
+  app.server?.publish(userId, JSON.stringify(message));
+}
 
 console.log(`Matching service is running at ${app.server?.hostname}:${app.server?.port}`);
