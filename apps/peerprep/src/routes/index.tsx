df2b12a759fed2a67dc5f32a796614944db53e5b
Vu Van Dung <me@joulev.dev>
2024-10-11 11:36:24 +0800

    wow websocket is really interesting
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/peerprep/src/routes/index.tsx
+++ apps/peerprep/src/routes/index.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import useSWRSubscription, { type SWRSubscriptionOptions } from "swr/subscription";
+
 export default function IndexPage() {
-  return <div>Welcome to PeerPrep!</div>;
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const { data } = useSWRSubscription("data", (_, { next }: SWRSubscriptionOptions<string>) => {
+    const ws = new WebSocket("ws://localhost:3004");
+    setWs(ws);
+    ws.addEventListener("message", event => next(null, event.data));
+    ws.addEventListener("error", () => next("Connection error", undefined));
+    return () => {
+      if (ws.readyState === 1) {
+        ws.close();
+      } else {
+        ws.addEventListener("open", () => {
+          ws.close();
+        });
+      }
+    };
+  });
+  return (
+    <div>
+      <button onClick={() => ws?.send(new Date().toISOString())}>Click me</button>
+      <div>{data ?? "nothingness"}</div>
+    </div>
+  );
 }
--- services/matching-service/src/index.ts
+++ services/matching-service/src/index.ts
@@ -1,12 +1,25 @@
 import { env } from "@peerprep/env";
-import { elysiaCorsPlugin, elysiaFormatResponsePlugin } from "@peerprep/utils/server";
+import {
+  elysiaAuthPlugin,
+  elysiaCorsPlugin,
+  elysiaFormatResponsePlugin,
+} from "@peerprep/utils/server";
 import { Elysia } from "elysia";
 
 const app = new Elysia()
   .use(elysiaCorsPlugin)
   .use(elysiaFormatResponsePlugin)
+  .use(elysiaAuthPlugin)
   .get("/status", () => new Response("Online"))
-  .get("/", () => new Response("Hello, world!"))
+  .ws("/", {
+    open(ws) {
+      ws.send("Welcome!");
+    },
+    message(ws, message) {
+      console.log(ws.id);
+      ws.send(`${message} ${ws.data.user?.email ?? "unauthenticated"}`);
+    },
+  })
   .listen(env.VITE_MATCHING_SERVICE_PORT);
 
 console.log(`Matching service is running at ${app.server?.hostname}:${app.server?.port}`);
