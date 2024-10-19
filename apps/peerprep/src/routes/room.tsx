5e6f4d3692fbda46f167c5568716676dcfc1779a
vivienherq <heruoqingvivien@gmail.com>
2024-10-19 21:12:11 +0800

    update monaco editor

--- apps/peerprep/src/routes/room.tsx
+++ apps/peerprep/src/routes/room.tsx
@@ -1,3 +1,4 @@
+import { HocuspocusProvider } from "@hocuspocus/provider";
 import Editor from "@monaco-editor/react";
 import { Button } from "@peerprep/ui/button";
 import { MarkdownRenderer } from "@peerprep/ui/markdown-renderer";
@@ -5,6 +6,16 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { useQuestion } from "@peerprep/utils/client";
 import { useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+import { MonacoBinding } from "y-monaco";
+import * as Y from "yjs";
+
+const ydoc = new Y.Doc();
+const provider = new HocuspocusProvider({
+  url: "ws://localhost:4000",
+  name: "test",
+  document: ydoc,
+});
+const ytext = ydoc.getText("monaco");
 
 export default function RoomPage() {
   const { id } = useParams<{ id: string }>();
@@ -26,7 +37,7 @@ export default function RoomPage() {
 
   return (
     <div className="flex h-full w-full gap-6">
-      <div className="bg-main-900 flex w-full flex-grow flex-col gap-6 overflow-auto p-12">
+      <div className="bg-main-900 flex w-1/2 flex-grow flex-col gap-6 overflow-auto p-12">
         <p>Room ID: {id}</p>
         {matched && <p>Matched Users: {matched.join(", ")}</p>}
         {questionId && <p>Question ID: {questionId}</p>}
@@ -36,7 +47,7 @@ export default function RoomPage() {
         </div>
       </div>
 
-      <div className="bg-main-900 flex w-full flex-grow flex-col px-4 py-4">
+      <div className="bg-main-900 flex w-1/2 flex-grow flex-col px-4 py-4">
         <div className="flex-grow overflow-auto">
           <Editor
             height="100%"
@@ -44,7 +55,12 @@ export default function RoomPage() {
             // defaultLanguage="javascript"
             language={language}
             defaultValue="// some comment"
-            // onMount={handleEditorDidMount}
+            onMount={editor => {
+              const editorModel = editor.getModel();
+              if (!editorModel)
+                throw new Error("invariant: monaco editor model is null, this shouldn't happen");
+              new MonacoBinding(ytext, editorModel, new Set([editor]), provider.awareness);
+            }}
           />
         </div>
         <div className="mt-4 grid grid-cols-2 gap-48">
