efa229e2bcaa90deb9f4243776998690d845d76c
vivienherq <heruoqingvivien@gmail.com>
2024-10-19 21:49:34 +0800

    create tabs

---
+++ packages/ui/tabs.tsx
@@ -0,0 +1,50 @@
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import { forwardRef } from "react";
+
+import { cn } from "./cn";
+
+const Tabs = TabsPrimitive.Root;
+
+const TabsList = forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn("border-main-800 flex border-b", className)}
+    {...props}
+  />
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
+
+const TabsTrigger = forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={cn(
+      "text-main-400 border-transparent px-4 py-2 transition-colors hover:border-white hover:text-white focus:text-white",
+      "data-[state=active]:border-b-2 data-[state=active]:border-white data-[state=active]:text-white",
+      className,
+    )}
+    {...props}
+  />
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+
+const TabsContent = forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <TabsPrimitive.Content
+    ref={ref}
+    className={cn("bg-main-900 text-main-400 mt-4 p-4", className)}
+    {...props}
+  >
+    {children}
+  </TabsPrimitive.Content>
+));
+TabsContent.displayName = TabsPrimitive.Content.displayName;
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
