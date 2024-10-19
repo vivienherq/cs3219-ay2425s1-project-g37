98f9370671b01f147165eacb71f003a92de956d3
vivienherq <heruoqingvivien@gmail.com>
2024-10-19 20:39:49 +0800

    add code editor to room

--- apps/peerprep/src/routes/room.tsx
+++ apps/peerprep/src/routes/room.tsx
@@ -1,5 +1,9 @@
+import Editor from "@monaco-editor/react";
+import { Button } from "@peerprep/ui/button";
 import { MarkdownRenderer } from "@peerprep/ui/markdown-renderer";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@peerprep/ui/select";
 import { useQuestion } from "@peerprep/utils/client";
+import { useRef, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
 export default function RoomPage() {
@@ -8,6 +12,13 @@ export default function RoomPage() {
 
   const location = useLocation();
   const { matched, questionId } = location.state || {};
+
+  const editorRef = useRef(null);
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  const [language, setLanguage] = useState("JavaScript");
 
   const { data: question } = useQuestion(questionId);
 
@@ -25,14 +36,38 @@ export default function RoomPage() {
       <div className="bg-main-900 flex w-full flex-grow flex-col gap-6 overflow-auto p-12">
         <p>Room ID: {id}</p>
         {matched && <p>Matched Users: {matched.join(", ")}</p>}
+        {questionId && <p>Question ID: {questionId}</p>}
         <h1 className="text-2xl font-semibold text-white">{question.title}</h1>
         <div className="prose prose-stone prose-invert max-w-full">
           {<MarkdownRenderer markdown={question.content} />}
         </div>
       </div>
 
-      <div className="bg-main-900 flex w-full flex-grow flex-col gap-6 p-12">
-        {questionId && <p>Question ID: {questionId}</p>}
+      <div className="bg-main-900 flex w-full flex-grow flex-col px-4 py-4">
+        <div className="flex-grow overflow-auto">
+          {/* Flex-grow wrapper for the editor */}
+          <Editor
+            height="100%" // Full height of the wrapper
+            theme="vs-dark"
+            // defaultLanguage="javascript"
+            language={language}
+            defaultValue="// some comment"
+            onMount={handleEditorDidMount}
+          />
+        </div>
+        <div className="mt-4 grid grid-cols-2 gap-48">
+          <Select value={language} onValueChange={value => setLanguage(value)}>
+            <SelectTrigger>
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="JavaScript">JavaScript</SelectItem>
+              <SelectItem value="Python">Python</SelectItem>
+              <SelectItem value="Java">Java</SelectItem>
+            </SelectContent>
+          </Select>
+          <Button variants={{ variant: "primary" }}>Run sample tests</Button>
+        </div>
       </div>
     </div>
   );
