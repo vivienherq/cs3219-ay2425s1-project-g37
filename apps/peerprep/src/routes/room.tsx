576aa10a68c352a99360d7774326cbe6561835aa
Vu Van Dung <me@joulev.dev>
2024-10-25 15:28:00 +0800

    make room page code cleaner
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/peerprep/src/routes/room.tsx
+++ apps/peerprep/src/routes/room.tsx
@@ -1,6 +1,7 @@
 import { HocuspocusProvider } from "@hocuspocus/provider";
 import Editor from "@monaco-editor/react";
 import { env } from "@peerprep/env";
+import type { Room, User } from "@peerprep/schemas";
 import { Avatar } from "@peerprep/ui/avatar";
 import { LinkButton } from "@peerprep/ui/button";
 import { Link } from "@peerprep/ui/link";
@@ -17,26 +18,65 @@ import * as Y from "yjs";
 
 import { NavAvatar } from "~/components/nav-avatar";
 import { NavLogo } from "~/components/nav-logo";
+import { generateContext } from "~/lib/generate-context";
 
 interface UserAwareness {
   username: string;
 }
+
+const ydoc = new Y.Doc();
+const [PageDataProvider, usePageData] = generateContext<{ user: User; room: Room }>("room data");
 
 function getTagString(tags: string[]) {
   if (tags.length >= 4) return `${tags.slice(0, 3).join(", ")}, +${tags.length - 3}`;
   return tags.join(", ");
 }
 
+function useHocuspocus() {
+  const { user, room } = usePageData();
+
+  const [stylesheets, setStylesheets] = useState<string[]>([]);
+  const [provider] = useState(() => {
+    const provider = new HocuspocusProvider({
+      url: `ws://localhost:${env.VITE_COLLABORATION_SERVICE_PORT}`,
+      name: room.id,
+      document: ydoc,
+      token: "dummy, required because hocuspocus needs it but we don't do anything with it",
+    });
+    provider.setAwarenessField("user", {
+      username: user.username,
+    } satisfies UserAwareness);
+    provider.on(
+      "awarenessUpdate",
+      ({ states }: { states: { clientId: number; user: UserAwareness }[] }) => {
+        const newStylesheets = states.map(({ clientId, user: awarenessUser }) => {
+          const colour = user.username === awarenessUser.username ? "#a855f7" : "#f59e0b";
+          const stylesheet = `
+      .yRemoteSelection-${clientId} {
+        background-color: ${colour}60;
+      }
+      .yRemoteSelectionHead-${clientId} {
+        position: absolute;
+        border-left: ${colour} solid 2px;
+        border-top: ${colour} solid 2px;
+        border-bottom: ${colour} solid 2px;
+        height: 100%;
+        box-sizing: border-box;
+      }
+    `.trim();
+          return stylesheet;
+        });
+        setStylesheets(newStylesheets);
+      },
+    );
+    return provider;
+  });
+  return { provider, stylesheets };
+}
+
 function Navbar() {
-  const { id } = useParams<{ id: string }>();
-  if (!id) throw new Error("invariant: id is undefined");
-
-  const { data: user } = useAuth();
-  const { data: room } = useRoom(id);
-  if (!user || !room) return null;
-
+  const { user, room } = usePageData();
   const collaborator = room.users[0].id === user.id ? room.users[1] : room.users[0];
-
   return (
     <nav className="flex flex-row justify-between p-6">
       <div className="flex flex-row items-center gap-6">
@@ -75,18 +115,9 @@ function Navbar() {
   );
 }
 
-export default function RoomPage() {
-  const { id } = useParams<{ id: string }>();
-  if (!id) throw new Error("invariant: id is undefined");
-
-  const { data: user } = useAuth();
-  const { data: room, error } = useRoom(id);
-
-  const [stylesheets, setStylesheets] = useState<string[]>([]);
-
-  if (error) return <Navigate to="/" />;
-  if (!user || !room) return null;
-
+function MainRoomPage() {
+  const { room } = usePageData();
+  const { provider, stylesheets } = useHocuspocus();
   return (
     <div className="flex h-screen w-screen flex-col px-6">
       <Navbar />
@@ -106,7 +137,7 @@ export default function RoomPage() {
             </div>
           </TabsContent>
           <TabsContent value="chat" className="flex-grow overflow-y-auto p-6 pt-0">
-            <p>Room ID: {id}</p>
+            <p>Room ID: {room.id}</p>
           </TabsContent>
         </Tabs>
         <div className="bg-main-900 flex flex-col gap-6 p-6">
@@ -141,41 +172,6 @@ export default function RoomPage() {
                 const editorModel = editor.getModel();
                 if (!editorModel)
                   throw new Error("invariant: monaco editor model is null, this shouldn't happen");
-                const ydoc = new Y.Doc();
-                const provider = new HocuspocusProvider({
-                  url: `ws://localhost:${env.VITE_COLLABORATION_SERVICE_PORT}`,
-                  name: id,
-                  document: ydoc,
-                  token:
-                    "dummy, required because hocuspocus needs it but we don't do anything with it",
-                });
-                provider.setAwarenessField("user", {
-                  username: user.username,
-                } satisfies UserAwareness);
-                provider.on(
-                  "awarenessUpdate",
-                  ({ states }: { states: { clientId: number; user: UserAwareness }[] }) => {
-                    const newStylesheets = states.map(({ clientId, user: awarenessUser }) => {
-                      const colour =
-                        user.username === awarenessUser.username ? "#a855f7" : "#f59e0b";
-                      const stylesheet = `
-                  .yRemoteSelection-${clientId} {
-                    background-color: ${colour}60;
-                  }
-                  .yRemoteSelectionHead-${clientId} {
-                    position: absolute;
-                    border-left: ${colour} solid 2px;
-                    border-top: ${colour} solid 2px;
-                    border-bottom: ${colour} solid 2px;
-                    height: 100%;
-                    box-sizing: border-box;
-                  }
-                `.trim();
-                      return stylesheet;
-                    });
-                    setStylesheets(newStylesheets);
-                  },
-                );
                 const ytext = ydoc.getText("monaco");
                 new MonacoBinding(ytext, editorModel, new Set([editor]), provider.awareness);
               }}
@@ -184,5 +180,21 @@ export default function RoomPage() {
         </div>
       </div>
     </div>
+  );
+}
+
+export default function RoomPage() {
+  const { id } = useParams<{ id: string }>();
+  if (!id) throw new Error("invariant: id is undefined");
+
+  const { data: user } = useAuth();
+  const { data: room, error } = useRoom(id);
+
+  if (error) return <Navigate to="/" />;
+  if (!user || !room) return null;
+  return (
+    <PageDataProvider value={{ user, room }}>
+      <MainRoomPage />
+    </PageDataProvider>
   );
 }
